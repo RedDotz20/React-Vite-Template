eaa3fcc682b93521ee48bd2f12226f82485f8316
RedDotz20 <carlos_tabangay@outlook.com>
2023-07-27 13:47:30 +0800

    feat: tailwind-css add

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-	return <div className="App">REACT TEMPLATE</div>;
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-center p-2">
+			<h1 className="font-semibold">Vite React Template</h1>
+		</main>
+	);
 }
 
 export default App;
