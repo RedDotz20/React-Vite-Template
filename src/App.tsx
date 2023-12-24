46db7370c0ce2a203e1e4aa892b30623830de405
RedDotz20 <carlos_tabangay@outlook.com>
2023-12-24 20:00:36 +0800

    feat: folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-function App() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-center p-2">
-			<h1 className="font-semibold">Vite React Template</h1>
-		</main>
-	);
+export default function App() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-2">
+      <h1 className="font-semibold">Vite React Template</h1>
+    </div>
+  );
 }
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './styles/index.css';
+
+import { ErrorBoundery } from './components/ErrorBoundery';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+  <React.StrictMode>
+    <ErrorBoundery fallback={<h1>An Error Has Occured</h1>}>
+      <App />
+    </ErrorBoundery>
+  </React.StrictMode>
 );
